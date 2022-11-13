2093cd5109de6a9a13ca39d67491a17877a518f6
Kallinteris Andreas <kallinteris@protonmail.com>
2022-11-13 21:25:52 +0200

    cleanup and factorizations

--- multiagent_mujoco/mujoco_multi.py
+++ multiagent_mujoco/mujoco_multi.py
@@ -8,29 +8,28 @@ from .obsk import get_joints_at_kdist, get_parts_and_edges, build_obs
 
 class MujocoMulti(pettingzoo.utils.env.ParallelEnv):
     def __init__(self, scenario: str, agent_conf: str, agent_obsk: int, render_mode: int=None):
-        self.scenario = scenario + '-v4'
+        scenario += '-v4'
 
-        self.agent_partitions, self.mujoco_edges, self.mujoco_globals = get_parts_and_edges(self.scenario, agent_conf)
+        self.agent_action_partitions, mujoco_edges, self.mujoco_globals = get_parts_and_edges(scenario, agent_conf)
 
-        #Petting Zoo API
-        self.possible_agents = [str(agent_id) for agent_id in range(len(self.agent_partitions))]
+        self.possible_agents = [str(agent_id) for agent_id in range(len(self.agent_action_partitions))]
         self.agents = self.possible_agents
 
         self.agent_obsk = agent_obsk # if None, fully observable else k>=0 implies observe nearest k agents or joints
 
         if self.agent_obsk is not None:
-            if self.scenario in ["Ant-v4", "manyagent_ant"]:
-                self.k_categories_label = "qpos,qvel,cfrc_ext|qpos"
-            elif self.scenario in ["Humanoid-v4", "HumanoidStandup-v4"]:
-                self.k_categories_label = "qpos,qvel,cfrc_ext,cvel,cinert,qfrc_actuator|qpos"
-            elif self.scenario in ["Reacher-v4"]:
-                self.k_categories_label = "qpos,qvel,fingertip_dist|qpos"
-            elif self.scenario in ["coupled_half_cheetah"]:
-                self.k_categories_label = "qpos,qvel,ten_J,ten_length,ten_velocity|"
+            if scenario in ["Ant-v4", "manyagent_ant"]:
+                k_categories_label = "qpos,qvel,cfrc_ext|qpos"
+            elif scenario in ["Humanoid-v4", "HumanoidStandup-v4"]:
+                k_categories_label = "qpos,qvel,cfrc_ext,cvel,cinert,qfrc_actuator|qpos"
+            elif scenario in ["Reacher-v4"]:
+                k_categories_label = "qpos,qvel,fingertip_dist|qpos"
+            elif scenario in ["coupled_half_cheetah"]:
+                k_categories_label = "qpos,qvel,ten_J,ten_length,ten_velocity|"
             else:
-                self.k_categories_label = "qpos,qvel|qpos"
+                k_categories_label = "qpos,qvel|qpos"
 
-            k_split = self.k_categories_label.split("|")
+            k_split = k_categories_label.split("|")
             self.k_categories = [k_split[k if k < len(k_split) else -1].split(",") for k in range(self.agent_obsk+1)]
 
             self.global_categories = []
@@ -38,32 +37,30 @@ class MujocoMulti(pettingzoo.utils.env.ParallelEnv):
 
         if self.agent_obsk is not None:
             self.k_dicts = [get_joints_at_kdist(agent_id,
-                                                self.agent_partitions,
-                                                self.mujoco_edges,
+                                                self.agent_action_partitions,
+                                                mujoco_edges,
                                                 k=self.agent_obsk,
                                                 kagents=False,) for agent_id in range(self.num_agents)]
 
         # load scenario from script
         try:
-            self.env = (gymnasium.make(self.scenario, render_mode=render_mode))
+            self.env = (gymnasium.make(scenario, render_mode=render_mode))
         except gymnasium.error.Error:  # env not in gymnasium
-            assert False, 'non Gymnasium Enviroment have not been implamented'
-            if self.scenario in ["manyagent_ant-v4"]:
+            assert False, 'Non-Gymnasium Enviroments have not been implamented'
+            if scenario in ["manyagent_ant-v4"]:
                 from .manyagent_ant import ManyAgentAntEnv as this_env
-            elif self.scenario in ["manyagent_swimmer-v4"]:
+            elif scenario in ["manyagent_swimmer-v4"]:
                 from .manyagent_swimmer import ManyAgentSwimmerEnv as this_env
-            elif self.scenario in ["coupled_half_cheetah-v4"]:
+            elif scenario in ["coupled_half_cheetah-v4"]:
                 from .coupled_half_cheetah import CoupledHalfCheetah as this_env
             else:
                 raise NotImplementedError('Custom env not implemented!')
-            env_args = {"scenario": self.scenario, "agent_conf": agent_conf, "agent_obsk": agent_conf}
             self.env = (agent_conf)#TODO add compatability
 
-        #Petting ZOO API
         self.observation_spaces, self.action_spaces = {}, {}
-        for a, partition in enumerate(self.agent_partitions):
-            self.action_spaces[a] = gymnasium.spaces.Box(low=-1, high=1, shape=(len(partition),), dtype=numpy.float32) #TODO LH
-            self.observation_spaces[a] = gymnasium.spaces.Box(low=-1, high=1, shape=(len(self._get_obs_agent(a)),), dtype=numpy.float32) #TODO LH
+        for agent_id, partition in enumerate(self.agent_action_partitions):
+            self.action_spaces[agent_id] = gymnasium.spaces.Box(low=-1, high=1, shape=(len(partition),), dtype=numpy.float32) #TODO LH
+            self.observation_spaces[agent_id] = gymnasium.spaces.Box(low=-1, high=1, shape=(len(self._get_obs_agent(agent_id)),), dtype=numpy.float32) #TODO LH
 
         pass
 
@@ -86,7 +83,7 @@ class MujocoMulti(pettingzoo.utils.env.ParallelEnv):
     def _map_actions(self, actions: dict[str, numpy.float32]):
         'Maps actions back into MuJoCo action space'
         env_actions = numpy.zeros((self.env.action_space.shape[0],)) + numpy.nan
-        for agent_id, partition in enumerate(self.agent_partitions):
+        for agent_id, partition in enumerate(self.agent_action_partitions):
             for i, body_part in enumerate(partition):
                 assert numpy.isnan(env_actions[body_part.act_ids]), "FATAL: At least one env action is doubly defined!"
                 env_actions[body_part.act_ids] = actions[str(agent_id)][i]
