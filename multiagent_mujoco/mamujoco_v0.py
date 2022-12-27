7ab3009c6ea5e6d516325ac01afc456b1f70497f
Kallinteris Andreas <kallinteris@protonmail.com>
2022-12-27 20:31:01 +0200

    pre-commit

--- multiagent_mujoco/mamujoco_v0.py
+++ multiagent_mujoco/mamujoco_v0.py
@@ -1,8 +1,7 @@
-from gymnasium_robotics.envs.multiagent_mujoco.mujoco_multi import (  # noqa : F401
+from gymnasium_robotics.envs.multiagent_mujoco.mujoco_multi import (  # noqa : F401;
     env,
     parallel_env,
     raw_parallel_env,
-    # MultiAgentMujocoEnv,  # This is exported for the Shinx Doc generator
 )
 
 # from gymnasium_robotics.envs.multiagent_mujoco.obsk import get_parts_and_edges
