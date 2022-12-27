06962b2c96bb4f5eff6e4b64e5144e7546bf3e1b
Kallinteris Andreas <kallinteris@protonmail.com>
2022-12-27 20:27:30 +0200

    cleanup

--- multiagent_mujoco/mamujoco_v0.py
+++ multiagent_mujoco/mamujoco_v0.py
@@ -1,9 +1,8 @@
 from gymnasium_robotics.envs.multiagent_mujoco.mujoco_multi import (  # noqa : F401
     env,
     parallel_env,
-    raw_env,
+    raw_parallel_env,
+    # MultiAgentMujocoEnv,  # This is exported for the Shinx Doc generator
 )
 
-# TODO export get_parts_and_edges
-# from multiagent_mojoco.obsk import get_parts_and_edges  # noqa : F401
-# from multiagent_mojoco.mujoco_multi import get_parts_and_edges  # noqa : F401
+# from gymnasium_robotics.envs.multiagent_mujoco.obsk import get_parts_and_edges
