fix `CoupledHalfCheetah` jacobian osbervations

multiagent_mujoco/coupled_half_cheetah.py
@@ -39,7 +39,7 @@ class CoupledHalfCheetah(mujoco_env.MujocoEnv, EzPickle):
         self._reset_noise_scale = 0.1
 
         observation_space = gymnasium.spaces.Box(
-            low=-np.inf, high=np.inf, shape=(54,), dtype=np.float32
+            low=-np.inf, high=np.inf, shape=(40,), dtype=np.float32
         )
 
         mujoco_env.MujocoEnv.__init__(
@@ -101,10 +101,11 @@ class CoupledHalfCheetah(mujoco_env.MujocoEnv, EzPickle):
         # NOTE: does not return tendon data
         return np.concatenate(
             [
-                self.data.qpos.flat[1:9],  # exclude rootx
-                self.data.qpos.flat[10:18],  # exclude rootx
+                self.data.qpos.flat[1:9],  # exclude rootx0
+                self.data.qpos.flat[10:18],  # exclude rootx1
                 self.data.qvel.flat,
-                self.data.ten_J[0],
+                self.data.ten_J[0][:2],
+                self.data.ten_J[0][9:11],
                 self.data.ten_length,
                 self.data.ten_velocity,
             ]

multiagent_mujoco/obsk.py
@@ -835,7 +835,9 @@ def get_parts_and_edges(  # noqa: C901
             0,
             tendons=(tendon),
             extra_obs={
-                "ten_J": lambda data: data.ten_J[tendon],
+                "ten_J": lambda data: np.concatenate(
+                    [data.ten_J[tendon][:2], data.ten_J[tendon][9:11]]
+                ),
                 "ten_length": lambda data: data.ten_length,
                 "ten_velocity": lambda data: data.ten_velocity,
             },
@@ -853,7 +855,9 @@ def get_parts_and_edges(  # noqa: C901
             6,
             tendons=(tendon),
             extra_obs={
-                "ten_J": lambda data: data.ten_J[tendon],
+                "ten_J": lambda data: np.concatenate(
+                    [data.ten_J[tendon][:2], data.ten_J[tendon][9:11]]
+                ),
                 "ten_length": lambda data: data.ten_length,
                 "ten_velocity": lambda data: data.ten_velocity,
             },